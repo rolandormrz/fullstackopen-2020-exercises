75a25bb9527c6ce96463ebb8f30ab1219f264e3e
Rolando Ramirez <rolandormrz@yahoo.com>
2020-04-25 23:57:41 -0400

    1.13*: anecdotes step 2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,18 +2,34 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({ 0: 0 });
 
   const handleClick = () => {
     const randomAnecdote = Math.floor(Math.random() * props.anecdotes.length);
     setSelected(randomAnecdote);
+    
+    if(typeof votes[randomAnecdote] === 'undefined') {
+      const newVotes = {...votes, [randomAnecdote]: 0};
+      setVotes(newVotes);
+    }
+  }
+
+  const handleVoteBtn = () => {
+    const newVotes = {...votes, [selected]: votes[selected] + 1};
+    setVotes(newVotes);
   }
 
   return (
     <div>
       <h1>Random Anecdote Randomizer</h1>
       <p>{props.anecdotes[selected]}</p>
-      <button onClick={handleClick}>Next Anecdote</button>
+      <p>Has {votes[selected]} votes</p>
+      <span>
+        <button onClick={handleVoteBtn}>Vote</button>
+        <button onClick={handleClick}>Next Anecdote</button>
+      </span>
+      
     </div>
   )
 }
--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -12,12 +12,6 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App anecdotes={anecdotes}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
 
 
