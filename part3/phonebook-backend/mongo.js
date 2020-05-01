a4cb3265679c9c9877c2892065e92eebb00f3943
Rolando Ramirez <rolandormrz@yahoo.com>
2020-05-01 13:55:31 -0400

    3.12: command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length < 3) {
+  console.log('Missing password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstackcourse:${password}@cluster0-sjkko.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if(process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    })
+    mongoose.connection.close();
+  })
+}
+else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name,
+    number
+  });
+
+  person.save().then(response => {
+    console.log(`${name} ${number} was added to the phonebook`);
+    mongoose.connection.close();
+  })
+}
